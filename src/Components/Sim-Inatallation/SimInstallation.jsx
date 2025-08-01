f51b053f11612b837ccbbb71be69eeede895015e
Shafqatraza494 <justinmartin67362@gmail.com>
2025-08-01 15:44:27 +0500

    usage card

--- src/Components/Sim-Inatallation/SimInstallation.jsx
+++ src/Components/Sim-Inatallation/SimInstallation.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import ButtonFill from "../GlobalUi/ui/Buttons/ButtonFill";
 import ButtonOutline from "../GlobalUi/ui/Buttons/ButtonOutline";
 import ButtonOutlineOrange from "../GlobalUi/ui/Buttons/ButtonOutlineOrange";
 import Direct from "./tabs/Direct";
 
 function SimInstallation() {
+  const [activeTab, setActiveTab] = useState("Data");
+  const [directData, setDirectData] = useState("direct");
   return (
     <>
       <div className="flex flex-col gap-10 px-18">
@@ -18,7 +22,27 @@ function SimInstallation() {
           <ButtonOutlineOrange text={"Android Devices"} />
         </div>
         <div>
+          <ul className="flex   mt-1 text-[22px] align-middle">
+            {["Direct", "QR Code", "Manual"].map((tab, index) => (
+              <li
+                key={index}
+                onClick={() => setActiveTab(tab)}
+                className={`cursor-pointer border-b-3   ${
+                  activeTab === tab
+                    ? "  w-[130px] border-[#EB662B] text-[#EB662B] font-[700]"
+                    : " w-[130px] text-black font-[400]"
+                }`}
+              >
+                {tab}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div>
           <Direct />
+          <h1>QR code</h1>
+          {directData === "qr" ? <p>QR Code</p> : null}
+          <h1>manual</h1>
         </div>
       </div>
     </>
