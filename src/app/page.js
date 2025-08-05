65ed7cdf8cc2709406b1434c3b2687571888ab35
Shafqatraza494 <justinmartin67362@gmail.com>
2025-08-05 17:50:52 +0500

    profile page

--- src/app/page.js
+++ src/app/page.js
@@ -5,18 +5,12 @@ import Footer from "@/Components/Footer/Footer";
 import Hero from "@/Components/Hero/Hero";
 import SimDetailsCard from "@/Components/GlobalUi/Cards/SimDetailsCard";
 import UsageDetailsCard from "@/Components/GlobalUi/Cards/UsageDetailsCard";
+import HeroHome from "@/Components/Home/HeroHome";
 
 function page() {
   return (
     <>
-      <Hero
-        para={
-          " Our team is here to assist you promptly through any of these channels."
-        }
-        heading={"Manage your sim here."}
-        btn1={"Buy eSIM"}
-        btn2={"My eSIM"}
-      />
+      <HeroHome />
     </>
   );
 }
